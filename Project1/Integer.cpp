48738821a56cf1d55d56940d3b433e31cbd914eb
weininglee <b10930003@mail.ntust.edu.tw>
2021-04-23 23:35:38 +0800

    Integer add overloading

--- Project1/Integer.cpp
+++ Project1/Integer.cpp
@@ -4,3 +4,45 @@ using std::reverse;
 bool Integer::test() {
 	return true;
 }
+
+Integer Integer::operator+(Integer plusInt) {
+	Integer sum;
+	
+	reverse(Int.begin(), Int.end());
+	reverse(plusInt.Int.begin(), plusInt.Int.end());
+	
+	int plus = 0, index = 0;
+	for (index; index < std::min(Int.size(), plusInt.Int.size()); index++) {
+
+		int x = (Int[index] - '0') + (plusInt.Int[index] - '0') + plus; // x = each digit sum
+		sum.Int += (char)(x % 10 + '0');
+		plus = x / 10;
+	}
+	if (Int.size() > plusInt.Int.size()) {
+		for (index = plusInt.Int.size(); index < Int.size(); index++) {
+			int x = (Int[index] - '0') + plus;
+			sum.Int += (char)(x % 10 + '0');
+			plus = x / 10;
+		}
+	}
+	else if (Int.size() < plusInt.Int.size()) {
+		for (index = Int.size(); index < plusInt.Int.size(); index++) {
+			int x = (plusInt.Int[index] - '0') + plus;
+			sum.Int += (char)(x % 10 + '0');
+			plus = x / 10;
+		}
+	}
+	if (plus) {
+		sum.Int += (char)(plus + '0');
+	}
+	reverse(sum.Int.begin(), sum.Int.end());
+	return sum;
+}
+
+Integer Integer::operator-(Integer plusInt) {
+
+}
+
+Integer Integer::operator*(Integer plusInt) {
+
+}
