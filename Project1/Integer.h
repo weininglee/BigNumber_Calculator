92a81b15c0ad4ef6e20d2d5ccce5129e987617b8
weininglee <b10930003@mail.ntust.edu.tw>
2021-04-23 17:46:03 +0800

    update integer add

--- Project1/Integer.h
+++ Project1/Integer.h
@@ -1,9 +1,21 @@
 #pragma once
-
+#include <iostream>
+#include <string>
 #include "NumberObject.h"
+
+using std::string;
 
 class Integer : NumberObject {
 public:
 	static bool test();
+
+	Integer();
+	Integer(string Int);
+
+	string Int;
+
+	Integer operator+(Integer plusInt);
+	Integer operator-(Integer plusInt);
+	Integer operator*(Integer plusInt);
 };
 
