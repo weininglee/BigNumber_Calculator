efa0d05b28a1bd340f7a90899fb33872d37c4ad1
weininglee <b10930003@mail.ntust.edu.tw>
2021-05-01 23:44:32 +0800

    update Integer

--- Project1/Integer.cpp
+++ Project1/Integer.cpp
@@ -245,7 +245,6 @@ bool Integer::operator<=(Integer compareInt) {
 }
 
 bool Integer::operator==(Integer compareInt) {
-
 	if (flag != compareInt.flag) return false;
 	if (Int.size() != compareInt.Int.size()) return false;
 
