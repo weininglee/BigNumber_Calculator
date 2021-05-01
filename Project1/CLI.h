ca995e8b1f321c805858a5ea6da887f038ad49d9
love96357410 <love96357410@gmail.com>
2021-05-01 21:36:02 +0800

    io

--- Project1/CLI.h
+++ Project1/CLI.h
@@ -28,7 +28,10 @@ public:
 			ci << cmd;
 			Integer a, b;
 			ci >> a >> b;
-			cout << "+= " << a + b << " -= " << a - b << " *= " << a * b /*<< " /=" << a / b */<< endl;
+			cout << "+= " << a + b << endl 
+				<< "-= " << a - b << endl 
+				<< "*= " << a * b << endl
+				<< "/= " << a / b << endl;
 			
 			cout << prefix;
 		}
