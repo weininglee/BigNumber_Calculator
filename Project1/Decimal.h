1a03d7bea8044eb7fbbb738c99f11069fc167577
CA-Lee <email@calee.tw>
2021-05-01 10:32:24 +0800

    add proto of Decimal

--- Project1/Decimal.h
+++ Project1/Decimal.h
@@ -1,35 +1,44 @@
 #pragma once
+#include "Integer.h"
 
-#include "NumberObject.h"
-
-class Decimal : NumberObject {
+class Decimal {
 public:
 
-	Integer denominator, numerator;
-	//Decimal power;
+	Integer denominator, numerator, power_denominator, power_numerator;
+	int sign;
 
 	Decimal();
 	Decimal(string);
 	Decimal(Decimal&);
+	Decimal(Decimal&&);
+	Decimal(Integer&);
+	Decimal(Integer&&);
 
 	Decimal operator+(Decimal);
 	Decimal operator-(Decimal);
 	Decimal operator*(Decimal);
 	Decimal operator/(Decimal);
-	Decimal operator+=(Decimal);
-	Decimal operator-=(Decimal);
-	Decimal operator*=(Decimal);
-	Decimal operator/=(Decimal);
+	Decimal& operator+=(Decimal);
+	Decimal& operator-=(Decimal);
+	Decimal& operator*=(Decimal);
+	Decimal& operator/=(Decimal);
 	Decimal operator-();
-	Decimal& operator=(Decimal);
+	Decimal& operator=(Decimal&);
+	Decimal& operator=(Decimal&&);
 	bool operator>(Decimal);
 	bool operator<(Decimal);
 	bool operator>=(Decimal);
 	bool operator<=(Decimal);
 	bool operator==(Decimal);
 	bool operator!=(Decimal);
-	//friend istream& operator>>(istream&, Decimal&);
-	//friend ostream& operator<<(ostream&, Decimal&);
+	friend istream& operator>>(istream& is, Decimal& to) { return is; }
+	friend ostream& operator<<(ostream& os, Decimal from) { return os; }
 	
+	bool is_int();
+	void simplefy(); // ¬ù¤À
+
+	static Decimal factorial(Decimal);
+	static Decimal power(Decimal, Decimal);
+
 	static bool test() { return false; }
 };
