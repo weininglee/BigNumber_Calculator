d4928d0d1385f0210c9f79e7efcf64fe4ab747a3
weininglee <b10930003@mail.ntust.edu.tw>
2021-05-01 21:20:11 +0800

    update constructor of Decimal

--- Project1/Decimal.cpp
+++ Project1/Decimal.cpp
@@ -1,5 +1,6 @@
 #include"Decimal.h"
-#include<iostream>
+#include <iostream>
+#include <iomanip>
 
 Decimal::Decimal() {
 	sign = 1;
@@ -10,7 +11,39 @@ Decimal::Decimal() {
 }
 
 Decimal::Decimal(string s) {
+	sign = 1;
+	if (s[0] == '-') sign = -1;
+	reverse(s.begin(), s.end());
+	bool isPoint = true;
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] == '.'){
+			denominator.Int.push_back(0);
+			denominator.Int[i] = 1;
+			isPoint = false;
+		}
+		else{
+			if (isPoint){
+				denominator.Int.push_back(0);
+				numerator.Int.push_back(s[i] - '0');
+			}
+			else{
+				numerator.Int.push_back(s[i] - '0');
+			}
+		}
+	}
+	for (int i = denominator.Int.size() - 1; i > 0; i--)
+	{
+		if (denominator.Int[i] == 0) denominator.Int.pop_back();
+		else break;
+	}
+	for (int i = numerator.Int.size() - 1; i > 0; i--)
+	{
+		if (numerator.Int[i] == 0) numerator.Int.pop_back();
+		else break;
+	}
 
+	if (denominator.Int.size() == 1 && denominator.Int[0] == 0) denominator.Int[0] = 1;
 }
 
 Decimal::Decimal(Decimal& from) {
@@ -195,8 +228,20 @@ bool Decimal::operator!=(Decimal compareDec) {
 	return true;
 }
 
-istream& operator>>(istream& is, Decimal& to) { return is; }
-ostream& operator<<(ostream& os, Decimal from) { return os; }
+istream& operator>>(istream& is, Decimal& to) {
+	string A;
+	is >> A;
+	to = Decimal(A);
+	return is;
+}
+
+ostream& operator<<(ostream& os, Decimal from) {
+	if (from.sign == -1)
+		os << '-';
+	Integer fromInt = from.numerator / from.denominator;
+	
+	return os; 
+}
 
 bool Decimal::is_int() {
 	Integer zero("0");
@@ -214,19 +259,42 @@ Integer Decimal::gcd(Integer a, Integer b) {
 	return b == zero ? a : gcd(b, a % b);
 }
 
-Decimal Decimal::factorial(Decimal) {
-
+Decimal Decimal::factorial(Decimal fac) {
+	bool check = fac.is_int();
+	if (check) {
+		Integer one("1");
+		//fac.numerator /= fac.denominator;
+		//fac.denominator = one;
+		fac.simplefy();
+		for (Integer i = fac.numerator - one; i > one; i -= one) {
+			fac.numerator *= i;
+		}
+		return fac;
+	}
+	else {
+		cout << "factorial error" << endl;
+		return fac;
+	}
 }
 
 Decimal Decimal::power(Decimal lower, Decimal upper) { // upper.denominator = 1 or 2
 	Integer one("1");
 	Integer multinum = lower.numerator;
 	Integer multiden = lower.denominator;
-	for (Integer count = upper.numerator; count > one; count -= one) {
-		lower.numerator *= multinum;
-		lower.denominator *= multiden;
+	if (upper.is_int()) {
+		for (Integer count = upper.numerator; count > one; count -= one) {
+			lower.numerator *= multinum;
+			lower.denominator *= multiden;
+		}
+		return lower;
+	}
+	else {
+		// ¶}®Ú¸¹
 	}
 
 }
 
-bool Decimal::test() { return false; }
+bool Decimal::test() { 
+
+	return false; 
+}
